f1399d44e784ec2c10510a1b14752998f6c333c1
Lucas Garcia <lukest64@gmail.com>
2022-01-18 15:50:02 +0000

    Added fade when transitioning slides

--- app.js
+++ app.js
@@ -7,18 +7,33 @@ const totalSlides = slides.length;
 document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
 document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
 
-function moveToPrevSlide() {
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
 
+function moveToPrevSlide() {
+    hideAllSlides();
+    if (slidePosition === 0) {
+        slidePosition = totalSlides -1;
+    } else {
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add('carousel-item-visible');
 }
 
 function moveToNextSlide() {
+    hideAllSlides();
     if (slidePosition === totalSlides -1) {
         slidePosition = 0;
     } else {
         slidePosition++;
     }
-}
 
+    slides[slidePosition].classList.add('carousel-item-visible');
+}
 
 
 console.log(totalSlides)
